040ef84f06f134f51d2564cb96cd5bf9d7ae34d3
sergejjarosch <sergejjarosch@gmail.com>
2024-04-04 11:27:57 +0200

    add Math

---
+++ src/math/Bruch.java
@@ -0,0 +1,26 @@
+package math;
+
+public class Bruch {
+    int zaehler;
+    int nenner;
+
+    void ausgeben(){
+        System.out.println(zaehler + "/" + nenner);
+    }
+    void kuerzen() {
+        var m = Math.abs(zaehler);
+        var n = Math.abs(nenner);
+        var r = m % n;
+        while (r > 0){
+            m = n;
+            n = r;
+            r = m % n;
+        }
+        zaehler /= n;
+        nenner /= n;
+    }
+    void gekuerztausgeben(){
+        kuerzen();
+        ausgeben();
+    }
+}
