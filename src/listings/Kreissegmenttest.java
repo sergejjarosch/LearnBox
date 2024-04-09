5ca23e4648d844f9246a234266ee25d4bdbda8b1
sergejjarosch <sergejjarosch@gmail.com>
2024-04-09 16:39:04 +0200

    add changes

--- src/listings/Kreissegmenttest.java
+++ src/listings/Kreissegmenttest.java
@@ -2,10 +2,13 @@ package listings;
 
 public class Kreissegmenttest {
     public static void main(String[] args) {
-        var segment = new Kreissegment();
+        var segment = new Kreissegment(2,180);
         System.out.println("Radius des Kreissegments: " + segment.getRadius());
         segment.radiusVergroesernUm(2);
         System.out.println("Radius des Kreissegments: " + segment.getRadius());
+        System.out.println("Fläche des Kreissegments: " + segment.getFlaeche());
+        System.out.println("Umfang des Kreissegments: " + segment.getUmfang());
+
 
     }
 }
